90a593ebf374c477fe462d973cdd704aabe6b0b4
LuaCarvalho <luacarvalho2008@hotmail.com>
2022-11-14 19:49:58 -0300

    Declarando variaveis

--- sintaxe-variaveis-e-fluxo/src/br/com/luacarvalho/alura/TestaVariaveis.java
+++ sintaxe-variaveis-e-fluxo/src/br/com/luacarvalho/alura/TestaVariaveis.java
@@ -5,6 +5,11 @@ public class TestaVariaveis {
 	public static void main(String[] ars) {
 		
 		System.out.println("Testa saída");
+		
+		int idade = 10;
+		
+		
+		System.out.println(idade);
 	}
 
 }
