3cb6a7322399b4c2072d1fdd613505eea8f83a92
LuaCarvalho <luacarvalho2008@hotmail.com>
2022-11-14 20:01:23 -0300

    Ponto Flutuante

--- sintaxe-variaveis-e-fluxo/src/br/com/luacarvalho/alura/TestaVariaveis.java
+++ sintaxe-variaveis-e-fluxo/src/br/com/luacarvalho/alura/TestaVariaveis.java
@@ -1,14 +1,11 @@
 package br.com.luacarvalho.alura;
 
 public class TestaVariaveis {
-	
+
 	public static void main(String[] ars) {
-		
+
 		System.out.println("Testa saída");
-		
 		int idade = 10;
-		
-		
 		System.out.println(idade);
 	}
 
